e31bdef9594f280203b2d85822ff0c11dd9d305d
vortex9 <vortex.jedi@gmail.com>
2014-07-01 16:59:32 +0300

    во втором меню добавлена поддржка font awesome

--- sites/all/themes/org/templates/page.tpl.php
+++ sites/all/themes/org/templates/page.tpl.php
@@ -12,7 +12,7 @@
 
   <header class="header" id="header" role="banner">
 	<?php if ($secondary_menu): ?>
-      <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
+      <nav class="fa header__secondary-menu" id="secondary-menu" role="navigation">
         <?php print theme('links__system_secondary_menu', array(
           'links' => $secondary_menu,
           'attributes' => array(
