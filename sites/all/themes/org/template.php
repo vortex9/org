b9c9e162e7b24960bc864acd4c941cdce408ee0a
vortex9 <vortex.jedi@gmail.com>
2014-07-10 18:33:29 +0300

    через шаблон изменён блок поиска

--- sites/all/themes/org/template.php
+++ sites/all/themes/org/template.php
@@ -130,3 +130,16 @@ function STARTERKIT_preprocess_block(&$variables, $hook) {
   //}
 }
 // */
+function org_form_alter(&$form, &$form_state, $form_id) {
+  if ($form_id == 'search_block_form') {
+    //$form['search_block_form']['#title'] = t('Поиск_label'); // Change the text on the label element
+    //$form['search_block_form']['#title_display'] = 'invisible'; // Toggle label visibilty
+    //$form['search_block_form']['#size'] = 40;  // define size of the textfield
+    $form['search_block_form']['#default_value'] = t('Поиск...'); // Set a default value for the textfield
+    $form['actions']['submit']['#value'] = html_entity_decode("&#xf002;", ENT_COMPAT, 'UTF-8'); // Change the text on the submit button
+    //$form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/search-button.png');
+	//$form['#attributes']['onsubmit'] = "if(this.search_block_form.value=='Введите текст...'){ alert('Please enter a search'); return false; }";
+   $form['actions']['submit']['#attributes']['class'][] = 'fa';
+  }
+  
+} 
